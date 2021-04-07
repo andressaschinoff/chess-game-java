42eb8cc192b179dd6b3a28937ad2e5b9fde64c48
--unset-all <--unset-all>
2021-04-07 11:14:33 -0300

    fix: change canMove to private and change order

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -17,11 +17,6 @@ public class King extends ChessPiece {
         return "K";
     }
 
-    public boolean canMove(Position position) {
-        ChessPiece p = (ChessPiece) getBoard().piece(position);
-        return p == null || p.getColor() != getColor();
-    }
-
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
@@ -44,6 +39,11 @@ public class King extends ChessPiece {
 
 
         return mat;
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
     }
 
     private void directionToMove(Position p, boolean[][] mat, Integer[] rowAndColumn) {
